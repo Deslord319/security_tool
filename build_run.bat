187c8e29146b805d49a913f94087607466d4bc81
mu <772927148@qq.com>
2026-03-11 13:27:44 +0800

    Remove local build scripts and signed artifacts

--- build_run.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set JAVA_HOME=D:\develop\DevEco Studio\jbr
-set PATH=%JAVA_HOME%\bin;%PATH%
-cd /d D:\project\ai\security_tool
-call node_modules\.bin\hvigorw.cmd assembleHap --mode module -p product=default -p module=entry@default
--- build_with_java.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set PATH=D:\develop\DevEco Studio\jbr\bin;%PATH%
-cd /d D:\project\ai\security_tool
-node "D:\develop\DevEco Studio\tools\hvigor\bin\hvigorw.js" assembleHap --mode module -p product=default -p module=entry@default
-echo Build completed with exit code %ERRORLEVEL%
-pause
